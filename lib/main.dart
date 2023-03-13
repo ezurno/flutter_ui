6229f4a7396abdb629594a12320bbb87f04d41a9
ezurno <ezurno@gmail.com>
2023-03-13 20:53:09 +0900

    LEARN: Container
    1. Container 는 div tag 와 비슷함
    
    2. Container의 style 을 바꿔주려면 decoration 으로 줌

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class App extends StatelessWidget {
           padding: EdgeInsets.symmetric(
               horizontal: 20), // symetric은 padding의 위치를 따로 지정 가능
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -46,6 +47,53 @@ class App extends StatelessWidget {
                       ),
                     ],
                   ),
+                ],
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "\$ 5 642 375",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 44,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    // Container 는 div 와 비슷함 style을 줄땐 decoration으로 줌
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(45),
+                      color: Colors.amber,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ],
